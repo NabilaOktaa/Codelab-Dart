371cd81b7770feac5388a27c76d4a46e6378d317
NabilaOktaa <101055397+NabilaOktaa@users.noreply.github.com>
2022-03-06 23:11:13 +0700

    1. Simple Dart Bycicle

---
+++ Bycicle.dart
@@ -0,0 +1,20 @@
+// 1. simple dart
+class Bicycle {
+  int cadence;
+  int _speed = 4;
+  int get speed => _speed;
+  int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+}
